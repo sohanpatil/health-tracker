a623f847121fd12b497d2a53061ec2034066664d
Sohan Patil <sohanpatil@outlook.com>
2020-05-18 15:42:34 -0700

    myfitnesspal done

--- start.py
+++ start.py
@@ -24,9 +24,30 @@ def add_header(r):
 
 @app.route("/", methods = ['POST', 'GET'])
 def home():
-    global sohanpatil
+    import datetime 
+    global client
+    # # get last week's calorie and water intake
+    global lastWeekCalorie
+    global lastWeekWater
+    lastWeekCalorie = []
+    for i in range(7):
+        tod = datetime.datetime.now()
+        d = datetime.timedelta(days = i)
+        a = tod - d
+        #print(a.year, a.month, a.day)
+        day = client.get_date(a.year, a.month, a.day)
+        dayTotals = day.totals
+        dayTotals['date'] = str(a.month)+"-"+str(a.day)+"-"+str(a.year)
+        dayTotals['water'] = day.water
+        lastWeekCalorie.append(dayTotals)
+
     if request.method == 'GET':
         return render_template("index.html")
+
+@app.route('/getMFPWateCalorierData', methods=['GET', 'POST'])
+def getMFPWaterData():
+    global lastWeekCalorie # have merged water data into it
+    return json.dumps(lastWeekCalorie)
 
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
@@ -49,5 +70,7 @@ def testConnection():
 if __name__ == '__main__':
 
     global client
+    global lastWeekCalorie
+    global lastWeekWater
     app.run(debug=True)
     app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
---
+++ static/js/myfitnesstrack.js
@@ -0,0 +1,107 @@
+
+$(document).ready(function() {
+    getWaterCaloriesAjax();
+});
+
+function getWaterCaloriesAjax() {
+    //dataset is global variable depicting selected dataset
+    $.get("getMFPWateCalorierData", function(data, status) {
+        if (data != "error") {
+            lastWeekData = []
+            var obj = $.parseJSON(data);
+            for (var key in obj) {
+                
+                //if (obj[0].hasOwnProperty(key)) {
+                    console.log(JSON.stringify(obj[key]));
+                    lastWeekData.push({
+                        "calories": parseFloat(obj[key]['calories']),
+                        "water": parseFloat(obj[key]['water']),
+                        "date": obj[key]['date']
+                    });
+                //}
+            }
+            plot(lastWeekData);
+        }
+    });
+}
+
+function plot(lastWeekData){
+    
+
+    var margin = {top: 30, right: 40, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
+
+    var parseDate = d3.time.format("%m-%d-%Y").parse;
+
+    var x = d3.time.scale().range([0, width]);
+    var y0 = d3.scale.linear().range([height, 0]);
+    var y1 = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x)
+        .orient("bottom").ticks(5);
+
+    var yAxisLeft = d3.svg.axis().scale(y0)
+        .orient("left").ticks(5);
+
+    var yAxisRight = d3.svg.axis().scale(y1)
+        .orient("right").ticks(5); 
+
+    var valueline = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y0(d.water); }); //close
+        
+    var valueline2 = d3.svg.line()
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y1(d.calories); }); //open
+    
+    var svg = d3.select("#myfitnesspal_chart")
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")");
+
+    // Get the data
+    var data = lastWeekData;
+    console.log(data);
+    data.forEach(function(d) {
+        console.log("parsing");
+        d.date = parseDate(d.date);
+        d.calories = +d.calories;
+        d.water = +d.water;
+    });
+
+    console.log("saafe"+JSON.stringify(data));
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.date; }));
+    y0.domain([0, d3.max(data, function(d) {
+		return Math.max(d.water); })]); 
+    y1.domain([0, d3.max(data, function(d) { 
+		return Math.max(d.calories); })]);
+
+    svg.append("path")        // Add the valueline path.
+        .attr("d", valueline(data));
+
+    svg.append("path")        // Add the valueline2 path.
+        .style("stroke", "red")
+        .attr("d", valueline2(data));
+
+    svg.append("g")            // Add the X Axis
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .style("fill", "steelblue")
+        .call(yAxisLeft);	
+
+    svg.append("g")				
+        .attr("class", "y axis")	
+        .attr("transform", "translate(" + width + " ,0)")	
+        .style("fill", "red")		
+        .call(yAxisRight);
+}
